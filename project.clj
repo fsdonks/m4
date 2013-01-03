0f4a0758144fc11f053bf2e68be260e6ba2cd782
Tom Spoon <spoon@blah.net>
2013-01-03 17:15:45 -0500

    Added docjure dependency, as well as libs for excel.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject marathon "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure.contrib/standalone "1.3.0-alpha4"]])
+                 [org.clojure.contrib/standalone "1.3.0-alpha4"]
+                 [dk.ative/docjure "1.6.0-SNAPSHOT"]])
