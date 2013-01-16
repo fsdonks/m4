17f01e267ce8b474a5badec63e702a532e0504b9
Tom <tom@development>
2013-01-15 20:03:20 -0500

    project updated

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]
-				 [dk.ative/docjure "1.6.0-SNAPSHOT"]])
+                 [dk.ative/docjure "1.6.0-SNAPSHOT"]])
