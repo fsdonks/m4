d4909cdd2741873ef231e5d05e85a742cac3e4b1
Tom <tom@development>
2013-02-19 01:42:57 -0500

    Added puresim

---
+++ src/sim/puresim.clj
@@ -0,0 +1,10 @@
+;This is a scratch namespace for plotting ideas on how to incorporate functional
+;reactive programming into a simulation, specifically a discrete event sim.
+;Note....we want to separate the presentation from the model...specifically, 
+;the aim is to use functional programming (not necessarily reactive programming)
+;to compose a model of a simulation elegantly.  We define sets of reactive 
+;behaviors....and compose these behaviors to form a (generally time-based) 
+;simulation.  
+(ns sim.puresim)
+
+
