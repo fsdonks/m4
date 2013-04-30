58e7051e333b3c1db2e80868bb83c515a58ec850
Tom Spoon <spoon@blah.net>
2013-04-30 16:31:24 -0400

    Pulled initial ports of marathon system logic.

---
+++ src/marathon/sim/engine.clj
@@ -0,0 +1,284 @@
+(ns marathon.sim.engine)
+
+;'The TimeStep_Engine is basically the essence of Marathon.  It acts as a harness around the simulation
+;'state, and dictates the flow of control through the simulation.  At a high level of abstraction, the
+;'engine is a process that manages one or more threads of execution.  In effecting the simulation, the
+;'engine wakes each thread sequentially, to simulate the flow of units through a supply, the presence of
+;'demands, and the flow of units between supply and demand via fills.  As such, the engine serves as the
+;'causal backbone for every bit of logic executed during the course of the simulation.  The main method,
+;'EventStep_Marathon, displays the order of execution of each logical subsystem (or thread).
+;
+;'TOM change 10 Sep 2012
+;'repurposing engine to be a shell...it's just a minimal wrapper around SimState.
+;'Engine's entire purpose is to prosecute a simulation, and wrap some parameters.
+;'Really, we should push all the parameters into TimeStep_Parameters.
+
+
+;'Timestep_simulation from given parameters.
+;'Right now, we read parameters/demand from Excel Host
+
+;'Prep the system for day 0
+Private Sub InitializeSim(state As TimeStep_SimState, Optional lastday As Single)
+
+With state
+    
+;    'TOM Change 30 Aug 2012
+;    'EntityFactory.startstate SupplyManager
+    .EntityFactory.startstate .supplystore
+    
+;    'TOM Change 30 Aug -> shift to this next.  SWAP
+;    'MarathonOpFactory.startstate .supplystore, .parameters, .context
+    .timeStart = TimeValue(Now)
+    .parameters.Workstate = Workstates.Simulating
+    
+;    'Adjust the context so that it defines a finite time horizon for the simulation.
+;    'The last  day of the simulation is determined either by passing in a lastday, or
+;    'by allowing the context to set its own last day dynamically.
+    Set .context = SimLib.setTimeHorizon(1, MarathonEngine.guessLastDay(state, lastday), _
+                                            .context)
+        
+;    'TOM Change 17 Aug 2012
+;    'Tell observers to sync themselves to the simulation state.  Some observers need access
+;    'to the specific elements of the state.  This makes it easy (and indirect) to advertise the
+;    'state, and to allow the observers to link to it as needed.
+    MarathonEngine.NotifyWatches state, .context  'propogate information about the simulation....
+    If .interactive Then MarathonEngine.notifyUI DumbUI, .context 'let folks know if we have a UI
+        
+    InitializeControl .context  'establish a link to the engine for system-wide events.
+    
+;    'Already sampling on day 0.
+;    'trigger Sample, name, name, vbnullstring
+End With
+
+End Sub
+;'TOM Change 26 Oct -> decoupled output creation.
+Private Sub initializeOutput(simstate As TimeStep_SimState, Optional path As String)
+Call MarathonSetup.initializeOutput(simstate, path)
+End Sub
+
+;'TOM Change 18 Aug 2012
+;'registers simulation-wide events, like updateAll__ with the engine.
+;'Allows the engine to perform system-wide IO operations via its handler.
+Sub InitializeControl(ctx As TimeStep_SimContext)
+With ctx.events
+    If Not .GetObserver.clients.exists(name) Then
+        .AddListener name, Me, TimeStep_Msg.updateAllUnits
+    End If
+End With
+End Sub
+
+;'TOM Change 17 Aug 2012 -> control system to allow the engine to handle system-wide requests
+;'for things like updates, etc.
+Sub ControlIO(msgid As Long, Optional data As GenericPacket)
+If msgid = TimeStep_Msg.updateAllUnits Then
+    'TOM Change, relying on a binding to global simState.
+    MarathonOpSupply.updateALL data.t, simstate.supplystore, simstate.context
+Else
+    Err.Raise 101, , "No other events are explicitly handled..."
+End If
+
+End Sub
+
+;'The main engine of the Marathon simulation.  This function comprises a single-threaded, discrete event
+;'simulation.
+Function EventStepMarathon(Optional lastday As Single, _
+                                Optional state As TimeStep_SimState)
+Dim day As Single
+Dim replicationtime As Single 'something to keep time in between days...
+
+If state Is Nothing Then Set state = getSimState()
+Set simstate = state 'bind the global state for the engine as well, only used for control IO.
+
+;'TOM Change 26 October 2012
+;'Decided to decouple the initialization of output from the input simstate.
+;'This opens up some possibilities for controlling how we handle IO...Right now, it's
+;'effectively a relocation of the output manager's initialization logic (which used to
+;'happen as the final step of generating simstate.  The upshot is, we can generate simstate
+;'without generating output files or causing side-effects related to output.  That allows
+;'much more pre-processing of the input simstate, which is necessary for splitting a state
+;'into multiple runs.
+initializeOutput state
+
+;'Derive the last day if we don't know it.
+If lastday = 0 Then lastday = MarathonEngine.guessLastDay(state, 0)
+
+;'we are stepping through eventful days only .... Moving to push system for time, minimizing updates.
+;'want to ensure that there is an enabled supply and /or demand that will actually provide some meaningful
+;'simulation output.  If not, we should warn the user about missing data leading to the absence of "stuff"
+;'to simulate.
+InitializeSim state, lastday
+
+;'If the simulation state is feasible, then we simulate each eventful day using a composition of
+;'simulation systems in Marathon.  Each system acts in turn, changing pieces of the overall simulation
+;'state.  Some systems communicate with eachother, and have direct access to eachother for efficiency and
+;'clarity.
+If MarathonEngine.CanSimulate(state) Then
+    While KeepSimulating(state)
+;        'Updates the simulation scheduler to the next scheduled eventful time.
+        profile "AdvanceTime"
+        day = SimLib.advanceTime(state.context)
+        profile "AdvanceTime"
+    
+;        'Trigger beginning-of-day logic and notifications.
+        BeginDay day, state
+        
+;        'Trying to manage the supply with our supply op
+;        'Manage the supply of the simulation, updating unit positions where necessary.
+        profile "ManageSupply"
+;        'state.supplystore.ManageSupply day, state  'Move/Rotate Supply -> changed location.
+        MarathonOpSupply.ManageSupply day, state
+        profile "ManageSupply"
+        
+;        'Activate/DeActivate policies, also moves/rotates supply...
+;        'TOM change 30 Aug 2012 -> added a shim to allow simcontext.  Policy is handled entirely in module now.
+        profile "ManagePolicies"
+        MarathonOpPolicy.ManagePolicies day, state
+        profile "ManagePolicies"
+               
+        'Activate/DeActiveate demands.  Send home units that are at DeActivating demands.
+        profile "ManageDemand"
+;        'state.demandstore.ManageDemands CLng(day), state
+        MarathonOpDemand.ManageDemands day, state
+        profile "ManageDemand"
+        
+;        'Try to fill Active, Unfilled demands in priority order.  Moves units as needed to fill demands.
+        profile "FillDemands"
+        MarathonOpDemand.FillDemands day, state
+;        'state.demandstore.FillDemands CLng(day), state
+        profile "FillDemands"
+        
+;        'Ensure that any unused units in a follow-on status are allowed to re-enter their policy.
+;        'TOM Change 17 Sep 2012 -> using logic in module vs. method.
+;        'state.supplystore.ReleaseFollowOns
+        MarathonOpSupply.ManageFollowOns day, state
+                
+;        'End of day logic and notifications.  Triggers sampling, possibly truncates the simulation.
+        endday day, state, lastday 'Record End of Day
+        
+;        'Eliminate the set of changed demands in the demandstore.  Used for sampling purposes.
+;        'Could be handled elsewhere, maybe as an event handler.
+;        'state.demandstore.clearChanges <- eliminated old call to have a uniform function signature
+        MarathonOpDemand.ManageChangedDemands day, state
+    Wend
+        
+;    'Final simulation logic.  Triggers final sampling of the simulation state, as well as termination
+;    'notification.
+    finalize day, state
+Else
+    Err.Raise 101, , "There's nothing to simulate.  Check Supply, Demand, and Relations!"
+End If
+
+profile "EventStep"
+End Function
+
+
+;'Simulation termination logic.
+Public Sub finalize(t As Single, state As TimeStep_SimState)
+;'Shift the simulation period into a final period.  Forces sampling and any other cleanup actions, like
+;'computing final bog:Dwell ratios, truncating unit lifecycles, etc.
+;'state.policystore.ManagePolicies t, "Final", state
+
+;'TOM note -> It'd be nice for all of these guys to have an identical signature...
+MarathonOpPolicy.ManagePolicies t, state, "Final"
+
+;'Not vital, but notifies user we're in an output phase.
+state.parameters.Workstate = outputing
+logStatus "Processing Output"
+state.parameters.Workstate = terminating
+EnableScreenUpdates
+
+state.timeFinish = TimeValue(Now)
+;'Notify any other listeners that the simulation has terminated.  Usually used for cleanup operations.
+SimLib.triggerEvent TimeStep_Msg.Terminate, name, name, "SimulationOVER!", , state.context
+
+;'Excel only....should be independent.
+finalizeControlSheet state.timeStart, state.timeFinish
+;'Clear the simulation state global.
+Set simstate = Nothing
+End Sub
+
+
+
+
+
+;'Update Logic for beginning a day.  Broadcasts the beginning of the current day
+;'on the simulation context's event stream.  Also, if a GUI is involved, notifies
+;'listeners whether a user has paused the simulation.
+Private Sub BeginDay(day As Single, state As TimeStep_SimState)
+
+If state.interactive Then
+    DoEvents
+End If
+
+trigger TimeStep_Msg.BeginDay, name, name, "<-------- Begin Day " & day & " ---------->", _
+        SimulationLog, , list(day, SimLib.getNextTime(state.context)), state.context
+
+If pause = True Then
+    trigger TimeStep_Msg.PauseSimulation, name, name, "Simulation Paused", _
+        SimulationLog, , list(day, SimLib.getNextTime(state.context)), state.context
+End If
+
+End Sub
+
+
+;'End of day logic.  Logs the passing of the day, notifies listeners of a need to generate
+;'samples for the day, and possibly truncates the simulation early if criteria have been met.
+Private Sub endday(day As Single, state As TimeStep_SimState, Optional lastday As Single)
+
+logStatus "Processed day " & day & " of " & lastday & " of Simulation"
+
+If interactive Then
+    DoEvents
+End If
+;'trigger EndofDay, name, name, "<-------- End of Day" & day & " ---------->", SimulationLog
+trigger sample, name, name, "Sampling"
+;'TOM Change 13 August 2012 -> added a default to quarterly sample all unit cycles
+;'for bog dwell stats.
+
+If state.sampleCyclesOverTime Then _
+    sampleUnitCycles day, state, True
+;    'sampleUnitCycles day, SupplyManager.unitmap, True
+
+If state.truncateTime And state.foundTruncation Then
+    trigger all, name, name, "Truncated the simulation on day " & day & ", tfinal is now : " & SimLib.getFinalTime(state.context)
+    foundTruncation = False
+End If
+
+If pause = True Then
+    ui.interrupt
+End If
+
+trigger TimeStep_Msg.EndofDay, name, name, "<-------- End Day " & day & " ---------->", _
+        SimulationLog, , day, state.context
+
+End Sub
+
+;'This is a special event handler, where the TimeStep_Engine is notified of a need to sample
+;'all units in the supply.  This typically happens when a period change occurs, or some other
+;'sweeping event that requires a synchronization of all the units.  For dwell stats, we typically
+;'report a proxy record for units not utilized during a simulation period.  To do this, we have
+;'to sample the entire unit population, which this event handler does.
+;'TOM Change 13 Aug 2012
+Private Sub sampleUnitCycles(t As Single, state As TimeStep_SimState, Optional quarterly As Boolean, Optional units As Dictionary)
+Dim samplep As Boolean
+If quarterly Then
+    If (t - 1) Mod 90 = 0 Then
+        samplep = True
+        addTime t + 90 'request another sample in 90 days.  This really should be request event.
+    End If
+Else
+    samplep = True
+End If
+If samplep Then
+    'SupplyManager.updateALL t 'synchronize our unit stats
+    MarathonOpSupply.updateALL t, state.supplystore, state.context
+    trigger GetCycleSamples, name, name, "Sample all UIC Cycles.  This is a hack.", , , _
+            newdict("t", t, "uics", units)
+End If
+
+End Sub
+
+
+
+
+
---
+++ src/marathon/sim/policyio.clj
@@ -0,0 +1,243 @@
+(ns marathon.sim.policyio)
+
+'Data related to the policystore or TimeStep_ManagerOfPolicy, is actually quite disparate.  There are
+'multiple data sources that must be read to compose and initialize a policystore that has information on
+'simulation periods, rotational policies, and substitution relations.  As such, policyIO has enough code
+'to justify breaking it out into a separate module, for organizational sanity.
+
+'The functions in this module concern reading data regarding policies and converting it into internal data
+'structures.  Many of the records and other data forms Marathon uses serve as a specification or a list of
+'instructions that are intended to be parsed to build policy structures.  The following functions support
+'the transformation of records to various policystore related structures, such as relations, atomic
+'rotational policies, composite rotational policies, tables of policy records, etc.  Most of these functions
+'will be wrapped in higher level constructors, like tablesToPolicyStore, or policyStoreFromExcel, which
+'glue together the lower level IO and parsing functions for the end user.  Additional parsing functions,
+'such as JSON and Clojure data readers/writers, will also crop up here as needed.
+Option Explicit
+
+'(Type    Relation    Donor   Recepient   Cost    Enabled)
+Public Function recordToRelation(inrec As GenericRecord) As Collection
+Set recordToRelation = fieldvals(inrec, "Relation", "Donor", "Recepient", "Cost")
+End Function
+
+'TODO -> implement this bad boy!
+'(_   _    _  PolicyName  Template    MaxDwell    MinDwell    MaxBOG  StartDeployable StopDeployable  Overlap     Recovery    BOGBudget   Deltas  _)
+Public Function recordToPolicy(inrec As GenericRecord) As TimeStep_Policy
+Dim rdict As Dictionary
+Dim keylist As Collection
+Dim startdeployable As Long, stopdeployable As Long, overlap As Long, recovery As Long, bogbudget As Long, deltas As Dictionary
+Dim templatename As String
+
+With inrec
+    If .fields("Deltas") <> "{}" Then
+        Set deltas = getDictionary(.fields("Deltas")) 'expand the deltas...
+    End If
+    templatename = CStr(.fields("Template"))
+    If templatename = "Ghost" Then
+        Set recordToPolicy = RegisterGhostTemplate(CStr(.fields("PolicyName")), CLng(.fields("MaxBOG")), CSng(.fields("Overlap")))
+    Else
+        Set recordToPolicy = RegisterTemplate(templatename, CLng(.fields("MaxDwell")), CLng(.fields("MinDwell")), _
+                                              CLng(.fields("MaxBOG")), CLng(.fields("StartDeployable")), CLng(.fields("StopDeployable")), _
+                                              CLng(.fields("Overlap")), deltas, deployableSet(templatename))
+    End If
+    recordToPolicy.name = CStr(.fields("PolicyName"))
+End With
+
+End Function
+'Flag for policies that don't need have their deployable ranges set.  Only applies to MaxUtilization.
+Public Function deployableSet(templatename As String) As Boolean
+Select Case templatename
+    Case MarathonEnumsAndConstants.MaxUtilization, MarathonEnumsAndConstants.NearMaxUtilization, MarathonEnumsAndConstants.FFGMission
+        deployableSet = True
+    Case Else
+        deployableSet = False
+End Select
+
+
+End Function
+
+'generate a collection of relations from the table records
+Public Function tableToRelations(tbl As GenericTable) As Collection
+Dim rec As GenericRecord
+
+Set tableToRelations = New Collection
+
+While Not tbl.EOF
+    Set rec = tbl.getGenericRecord
+    If rec.fields("Enabled") Then tableToRelations.add recordToRelation(rec)
+    tbl.moveNext
+Wend
+
+End Function
+'generate a collection of atomic policies from the table records
+Public Function tableToPolicies(tbl As GenericTable) As Collection
+Dim rec As GenericRecord
+
+Set tableToPolicies = New Collection
+
+While Not tbl.EOF
+    Set rec = tbl.getGenericRecord
+    tableToPolicies.add recordToPolicy(rec)
+    tbl.moveNext
+Wend
+
+End Function
+'generate a dictionary of atomic policies from a table, where the keys are
+'policy names.  Enforces unique policy names.
+Public Function tableToPolicyDict(tbl As GenericTable) As Dictionary
+Dim rec As GenericRecord
+Dim p As TimeStep_Policy
+
+Set tableToPolicyDict = New Dictionary
+
+While Not tbl.EOF
+    Set rec = tbl.getGenericRecord
+    If Not tableToPolicyDict.exists(rec.fields("PolicyName")) Then
+        Set p = recordToPolicy(rec)
+        tableToPolicyDict.add p.name, p
+    End If
+    tbl.moveNext
+Wend
+
+End Function
+
+
+'Reads an expression from a record
+'with keys (CompositeName Policy), vals [somestring, {policy dictionary}/or [policy list]]
+'We use our evaluator to transform the policy string into a policy dictionary.
+'Returns a pair of [rulename, {policy dict}], or [rulename [policy sequence]]
+Public Function recordToComposition(inrec As GenericRecord) As Collection
+Dim rulename As String
+rulename = inrec.fields("CompositeName")
+Set recordToComposition = list(rulename, reval(CStr(inrec.fields("Composition"))))
+End Function
+'Generates a dictionary of composition rules from the table.
+'Keys in the dictionary correspond to the name of the rule, and vals correspond to
+'a map of period names to policy names/ policies.
+Public Function tableToCompositions(tbl As GenericTable) As Dictionary
+Dim rec As GenericRecord
+Dim rulename As String
+Dim entry As Collection
+Dim composition As Dictionary
+Dim policysequence As Collection
+Dim res As Collection
+Dim tmp()
+Dim subpolicies
+Dim itm
+
+Set tableToCompositions = New Dictionary
+
+While Not tbl.EOF
+    Set rec = tbl.getGenericRecord
+    If rec.fields.exists("Composition") Then 'this is a table of literal compositions, rather than flat recs.
+        Set res = recordToComposition(rec)
+        rulename = res(1) 'the name of sequential policy.
+        If tableToCompositions.exists(rulename) Then
+            Err.Raise 101, , "Composition for " & rulename & "Already exists!"
+        Else
+            Select Case TypeName(res(2))
+                Case "Collection"  'sequential policy
+                    Set policysequence = res(2) 'a list of policy names.
+                    tableToCompositions.add rulename, policysequence 'add the sequential policy as a valid composition rule.
+                Case "Variant()"
+                    Set policysequence = New Collection
+                    tmp = res(2)
+                    For Each itm In tmp
+                        policysequence.add itm
+                    Next itm
+                    tableToCompositions.add rulename, policysequence 'add the sequential policy as a valid composition rule.
+                Case "Dictionary"
+                    Set composition = res(2)
+                    tableToCompositions.add rulename, composition
+                Case Else
+                    Err.Raise 101, , "Unknown composite policy type!"
+            End Select
+        End If
+    Else
+        rulename = rec.fields("CompositeName")
+        Select Case rec.fields("CompositionType")
+            Case "Periodic"
+                If tableToCompositions.exists(rulename) Then
+                    Set composition = tableToCompositions.item(rulename)
+                Else
+                    Set composition = New Dictionary
+                    tableToCompositions.add rulename, composition
+                End If
+                composition.add rec.fields("Period"), rec.fields("Policy")
+            Case "Sequential"
+                If tableToCompositions.exists(rulename) Then
+                    Err.Raise 101, , "Composition " & rulename & " already exists!"
+                Else
+                    subpolicies = Split(CStr(rec.fields("Policy")), ",")
+                    Set policysequence = New Collection
+                    For Each itm In subpolicies
+                        policysequence.add Trim(CStr(itm))
+                    Next itm
+                    tableToCompositions.add rulename, policysequence
+                End If
+            Case Else
+                Err.Raise 101, , "Error parsing composition policy!" & rulename
+            End Select
+    End If
+    tbl.moveNext
+Wend
+
+End Function
+'Public Function compositionType(incomp As Dictionary) As PolicyType
+'Select Case TypeName(incomp(2))
+'End Function
+
+'much more robust, uses the generictable interface to simplify loading.
+Public Function tablesToPolicyStore(relationtable As GenericTable, periodtable As GenericTable, atomictable As GenericTable, compositeTable As GenericTable) As TimeStep_ManagerOfPolicy
+Dim ps As TimeStep_ManagerOfPolicy
+
+Set ps = New TimeStep_ManagerOfPolicy
+
+addRelations tableToRelations(relationtable), ps
+addPeriods PeriodLib.tableToPeriods(periodtable), ps
+addDependentPolicies tableToPolicyDict(atomictable), tableToCompositions(compositeTable), ps
+
+Set tablesToPolicyStore = ps
+Set ps = Nothing
+
+End Function
+
+
+'Returns a policystore object initialized from default tables in Excel.  Mostly for compatibility.
+Public Function policyStoreFromExcel() As TimeStep_ManagerOfPolicy
+Set policyStoreFromExcel = tablesToPolicyStore(getTable("RelationRecords"), _
+                             getTable("PeriodRecords"), _
+                             getTable("PolicyRecords"), _
+                             getTable("CompositePolicyRecords"))
+End Function
+Public Function policyStoreFromFiles() As TimeStep_ManagerOfPolicy
+Dim tbl
+Dim tblist As Collection
+Dim p As String
+p = ActiveWorkbook.path & "\"
+
+Set tblist = list("RelationRecords", "PeriodRecords", "PolicyRecords", "CompositePolicyRecords")
+For Each tbl In tblist
+    TableLib.saveTable getTable(CStr(tbl)), p & "\" & CStr(tbl) & ".json"
+Next tbl
+                             
+Set policyStoreFromFiles = tablesToPolicyStore(getTable(p & "RelationRecords" & ".json"), _
+                             getTable(p & "PeriodRecords" & ".json"), _
+                             getTable(p & "PolicyRecords" & ".json"), _
+                             getTable(p & "CompositePolicyRecords" & ".json"))
+
+End Function
+Public Function folderToPolicyStore(folderpath As String) As TimeStep_ManagerOfPolicy
+Dim tbl
+Dim tblist As Collection
+Dim p As String
+p = folderpath
+
+Set tblist = list("RelationRecords", "PeriodRecords", "PolicyRecords", "CompositePolicyRecords")
+Set folderToPolicyStore = tablesToPolicyStore(getTable(p & "RelationRecords" & ".json"), _
+                             getTable(p & "PeriodRecords" & ".json"), _
+                             getTable(p & "PolicyRecords" & ".json"), _
+                             getTable(p & "CompositePolicyRecords" & ".json"))
+                             
+End Function
+
