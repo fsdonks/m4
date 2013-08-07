0eead5958916be7df1e2b7ebfd84659ed6489c0c
Tom Spoon <spoon@blah.net>
2013-08-07 16:51:37 -0400

    added script to build uber doc via marginalia

---
+++ build-docs.bat
@@ -0,0 +1,1 @@
+lein marg src/marathon/sim/core.clj src/marathon/sim/engine.clj src/marathon/sim/fill.clj src/marathon/sim/demand.clj src/marathon/sim/supply.clj src/marathon/sim/policy.clj src/marathon/sim/policyio.clj
