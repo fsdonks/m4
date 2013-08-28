5931b9de703ed741b31f59857ad430375d893880
Tom Spoon <spoon@blah.net>
2013-08-28 19:16:45 -0400

    Eliminated docjure dependency, since we get it through spork now.

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]
-                 [dk.ative/docjure "1.6.0-SNAPSHOT"]
                  [spork "0.1.1-SNAPSHOT"]]
   :main marathon.core)
