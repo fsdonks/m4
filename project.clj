c083fd1263007a569d79d0682302f54c3206b3e1
Tom Spoon <spoon@blah.net>
2013-09-09 13:12:20 -0400

    Updated to new version of spork.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]
-                 [spork "0.1.5-SNAPSHOT"]]
+                 [spork "0.1.6-SNAPSHOT"]]
   :main marathon.core)
