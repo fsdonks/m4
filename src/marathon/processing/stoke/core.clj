c371d895646594c33b37d9dee65a1eb157b999ca
Tom Spoon <spoon@blah.net>
2013-09-18 17:46:36 -0400

    Got significant functionality for stoke.

--- src/marathon/processing/stoke/core.clj
+++ src/marathon/processing/stoke/core.clj
@@ -4,7 +4,9 @@
 ;;end-strength constrained portfolios.
 (ns marathon.processing.stoke.core
   (:require [spork.opt [core :as opt]
-                       [representation :as rep]]))
+                       [representation :as rep]]
+            [spork.util [combinatoric :as c]
+                        [generators :as gen]]))
 
 
 ;;The task we face in Stoke is to try to provide a fast approximation of a 
@@ -92,12 +94,116 @@
 
 ;;Our representation of supply. 
 
+;;An arbitrary upper bound on what would be a ludicrious amount of supply.
+(def ludicrous-amount 999999999)
+
+;;Some possible values for a notional supply.
+;;We have two capabilities:  
+;;MeatEaters, who can inflict violence with extreme prejudice.  
+;;ButterChurners, who perform a lot of labor intensive butter churning.  
+;;We also have to general classes, or components: the lifers, dudes that are 
+;;employed full-time in either profession, and part-timers or WeekendWarriors. 
+(def notional-stats 
+  {:src-strengths {:MeatEaters 20
+                   :ButterChurners 50} 
+   :compos [:Lifers :WeekendWarriors]})
+
+(def notional-srcs   (keys (:src-strengths notional-stats)))
+(def notional-compos (:compos notional-stats))
+
+(defn random-demand [srcs] 
+  {:Start (rand-int 4000) :Duration (rand-int 200) :SRC (rand-nth srcs)})
+
+;(def random-demands (take 10 (repeatedly #(random-demand notional-srcs)))) 
+
+;;A generation of random-demands.
+(def some-demands
+  [{:Start 6,    :Duration 165, :SRC :ButterChurners}
+   {:Start 3080, :Duration 46,  :SRC :MeatEaters}
+   {:Start 2135, :Duration 40,  :SRC :ButterChurners}
+   {:Start 2997, :Duration 152, :SRC :ButterChurners}
+   {:Start 2705, :Duration 170, :SRC :MeatEaters}
+   {:Start 3365, :Duration 170, :SRC :MeatEaters}
+   {:Start 1230, :Duration 193, :SRC :ButterChurners}
+   {:Start 347,  :Duration 178, :SRC :MeatEaters}
+   {:Start 815,  :Duration 4,   :SRC :MeatEaters}
+   {:Start 2045, :Duration 167, :SRC :ButterChurners}])
+
+(def small-demands (take 2 some-demands))
+
+
+;;given a sorted sequence of demands, of a identical SRC, we need a way 
+;;to walk the timeline, and accumulate a list of active demands.  The 
+;;only time the active demands change is when a new demand activates, 
+;;or an existing demand deactivates. We just sweep the demands in order 
+;;recording their start and stop times, and accumulate a sequence of 
+;;active demands by time. This will help us compute peaks easily later.
+(defn temporal-profile [xs] 
+  (let [add-demand  (fn [t x] {:t t :type :add  :data x})
+        drop-demand (fn [t x] {:t t :type :drop :data x})
+        resample    (fn [t]   {:t t :type :resampling :data nil})
+        earliest    (fn [l r] (compare (:t l) (:t r)))
+        handle (fn [{:keys [t type data]} [es actives state]]
+                  (case type
+                    :resampling [es actives :changed]
+                    :add [(-> es (conj (drop-demand (+ t (:Duration data)) data))
+                                 (conj (resample t)))
+                          (conj actives data)
+                          :added]
+                    :drop [es (disj actives data) :dropped]))
+        initial-events (into (sorted-set-by earliest) 
+                             (map (fn [x] (add-demand (:Start x) x)) xs))]
+  (gen/unfold (fn [[es _ _]]  (empty? es))  ;halt when no more events.            
+              (fn [[es actives s]]                
+                (let [event               (first es)
+                      remaining-events    (disj es event)
+                      current-time        (:t event)]
+                  (handle event [remaining-events actives s]))) 
+              [initial-events #{} :init])))
+
+;;Pretty general function.
+(defn activity-profile [xs]
+  (->> (temporal-profile xs)
+       (map (fn [[es actives s]] {:t (:t (first es)) :actives actives :s s}))
+       (partition-by :t)
+       (map last)
+       (concat)
+       (map (fn [x] [(:t x) (-> x (dissoc :t) 
+                                  (dissoc :s)
+                                  (assoc  :count (count (:actives x))))]))
+       (into (sorted-map)))) ;;sorted map may be gratuitous       
+
+(defn peak-demands [xs]
+  (let [active-count (fn [r] (:count r))
+        sorted  (->> (sort-by :Start xs)
+                  (activity-profile)
+                  (sort-by (fn [[t r]] (active-count r))) 
+                  (reverse))
+        peak (active-count (second (first sorted)))]
+    (take-while (fn [[t r]] (= (active-count r) peak))
+                sorted))) 
+
+;;given a sequence of demands, we need a way to compute peak demand.
+(defn get-peak-demands [xs] 
+  (for [[src recs] (group-by :SRC xs)]
+    (sort-by :Start recs) 
+
 ;;We'll derive the srcs from the actual demand later. 
 (defn ->supply [srcs compos end-strength]
-  {:solution (map vector (interleave srcs compos) (repeat 0))
+  {:solution (into {} (map vector (map vector srcs compos) (repeat 0)))
    :end-strength end-strength})
-      
+  
+(defn supply-spec [srcs compos]
+  (let [combos (for [s srcs
+                     c compos] [s c])] 
+    (reduce conj {}  
+       (for [[s c] combos]
+         [[s c] [0 ludicrous-amount]]))))
 
+(defmacro supply-solution [srcs compos]
+  (let [spec (supply-spec (eval srcs) (eval compos))]   
+    `(~'rep/defsolution ~'supply ~spec)))
+  
 
 
 
