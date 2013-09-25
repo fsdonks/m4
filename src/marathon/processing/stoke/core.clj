0921e2ca7c8c32ab0293d567e7595bd87bcfe71d
Tom Spoon <spoon@blah.net>
2013-09-25 15:50:37 -0400

    added API for stoke.

--- src/marathon/processing/stoke/core.clj
+++ src/marathon/processing/stoke/core.clj
@@ -226,10 +226,6 @@
                     compo  (demand->compo demand)     
                     surplus (surplus-strength acc)
                     {:keys [SRC Priority Quantity]} demand
-;                    _ (println {:mfs min-feasible-strength
-;                                :surplus surplus
-;                                :src SRC :pri Priority :qty Quantity                                
-;                                }) 
                     [filled spent] (try-fill surplus SRC Quantity)                   
                     new-supply (if (> filled 0)
                                  (add-supply acc SRC compo filled spent)
@@ -241,114 +237,12 @@
                           (advance-breaks next-min-feasible breaks dropped)] 
                     (recur new-supply (filter-feasibles new-dropped (rest xs)) 
                            new-breaks new-dropped feasibles))
-                  ;add supply and continue filling.
+                  ;continue filling, recording a successful fill.
                   (recur new-supply (rest xs) breaks dropped 
                          (conj feasibles demand))))))))
-
-;;Filling Approximately Optimally
-;;===============================
-
-;;Useful functions for optimizing
-(defn sum [xs] (reduce + xs))     
-(defn weighted-sum [key-vals key->weight]
-  (reduce (fn [acc [k v]] (+ acc (* v (key->weight k)))) 0 key-vals))
-  
-(defn sum-keys-by
-  "Given a map of {k quantity}, where quantity is a number, sums the map using 
-   a custom key function.  Used to aggregate maps."
-  [keyf m]
-  (->> (seq m)
-       (reduce (fn [acc [k qty]] (assoc! acc (+ (get acc (keyf k) 0) qty))) 
-               (transient {}))
-       (persistent!)))
-
-;;Generating supply is fairly straightfoward.  Since we're using clojure's data
-;;structure, we can share a lot of data instead of having to put it into array 
-;;based structures and shovel indices around.  
-
-;;We formulate a simple optimization, Given:      
-;;srcs   - a set of srcs,  
-;;compos - a set of components,  
-;;peaks  - a map of {src demand},  
-;;compo-pref - a map of {demand compo},
-;;priority   - a positive real number
-;;demand-priority - a map of {demand priority}
-;;src-strength - a map of {src strength},  
-;;max-end-strength - the maximum size of the supply, 
-
-;;with the following decision variables: 
-;;  
-;;The quantity of supply assigned by demand, component, and src:  
-;;supplied =  {[demand compo src] quantity}  
-;;total-fill = {src (sum (supplied [demand compo src])}
-;;size-of-supply =     
-;;  (sum (for [[src qty] total-fill] (* (src-strength src) qty))) 
-       
-;;with the following constraints:  
-;;  size-of-supply <= max-end-strength   
-;;  We'll implement this constraint via a severe penality on the objective 
-;   function for solutions that are over-strength. 
-;;  
-;;the following weight functions:  
-;;  (fill-weight qty src) = (* qty (src-strength src) (demand-priority src))  
-;;  (compo-weight demand compo) = (if (= (get compo-pref demand) compo) 1 0.5)
-;;and the following objectives:  
-;;  (weighted-fill supplied) = 
-;;       (sum (for [[[demand compo src] qty] supplied] 
-;;                (+ (fill-weight qty src) (compo-weight demand compo))))
-;;maximize (* (weighted-fill supplied) (strength-penalty supplied))
-
-;;Translated (directly) into a value function:
-(defn ->value-function
-  "We maximize the weighted fill of the supply, relative to the demand, demand 
-   preferences, and strengths, and penalize solutions that are over-strength 
-   by dividing the weighted-fill by the amount over strength.  Solutions that 
-   are within strength bounds suffer no penalty."
-  [max-end-strength compo->pref demand->priority src->strength]
-  (let [total-fill (fn [supplied] (sum-keys-by (fn [[_ _ src]] src)  supplied))
-        size-of-supply (fn [tot-fill] (weighted-sum tot-fill src->strength))
-        fill-weight    (fn [qty src] (* qty (src->strength src) 
-                                            (demand->priority src)))
-        compo-weight   (fn [demand compo] 
-                         (if (= (compo->pref demand) compo) 1 0.5))        
-        weighted-fill  (fn [supplied] 
-                         (sum (for [[[demand compo src] qty] supplied] 
-                                (+ (fill-weight qty src) 
-                                   (compo-weight demand compo)))))
-        strength-penality (fn [size] (/ 1.0 (min (- size max-end-strength) 1.0)))]
-    (fn [supplied] (* (weighted-fill supplied) 
-                      (strength-penality (size-of-supply supplied))))))
-
-;;We wrap everything up into a nice function that lets us build the optimization
-;;en toto.  Given a demand future, and an empty supply, we construct a 
-;;specification for an optimization problem that will generate a supply.
-
-;;(defn ->optimization [supply demand-future]
-
-(comment 
-(defn unkey [k] 
-  (if (keyword? k)
-    (subs (str k) 1)
-    k))
-
-(defn supply-spec [srcs compos]
-  (let [combos (for [s srcs
-                     c compos] (symbol (str (unkey s) "_"  (unkey c))))] 
-    (reduce conj {}  
-       (for [k combos]
-         [k [0 ludicrous-amount]]))))
-
-(defn supply-solution [srcs compos]
-  (let [spec (supply-spec srcs compos)]   
-    (eval `(defsolution ~'supply ~spec))))  
-)
-
 ;;Testing
 ;;=======
 
- 
-;;Shared Data
-;;===========
 
 ;;Some possible values for a notional supply.
 ;;We have two capabilities:  
@@ -361,7 +255,7 @@
 
 (def notional-stats 
   {:src-strengths {:MeatEaters 23 :ButterChurners 55} 
-   :compos [:Lifers :WeekendWarriors]})
+   :compos        [:Lifers :WeekendWarriors]})
 
 ;;Useful aliases 
 (def notional-srcs   (keys (:src-strengths notional-stats)))
@@ -377,7 +271,6 @@
 ;;a start time, a duration, an SRC or capability type, and a quantity. Standard
 ;;demand records already codify this, and provide additional meta data (for more
 ;;robust value functions) if desired.
-
 
 (def compo-pref {:MeatEaters      :Lifers
                  :ButterChurners  :WeekendWarriors})
@@ -417,6 +310,50 @@
 (def filled-supply 
   (hierarchically-fill-supply empty-supply demand-future)) 
 
+
+
 )
-                                     
+
+;;High Level API 
+;;==============
+;;We'll wrap this up in a nice package...
+
+
+;;I am currently wrapping the glue code here to tie in the  value function.  
+
+(defn supply->portfolio
+  "Generates a portfolio of n force structures, evaluated against k 
+   demand futures, drawn from a demand generation function."
+  [initial-supply & {:keys [n supply-gen demand-gen] 
+                     :or   {n 10
+                            supply-gen hierarchically-fill-supply
+                            demand-gen random-demands}}]
+  'currently-implementing)
+
+(defn portfolio->performance 
+  "Given a portfolio of x supplies and a sample size, generates k 
+   demand futures, and evaluates each supply against them."
+  [xs futures & {:keys [value-func]  :or {value-func evaluate-supply}}]
+  (let [indexed-supply  (map-indexed vector xs)
+        indexed-futures (map-indexed vector futures)                           
+        results     (for [[i supply] indexed-supply
+                          [j demand] indexed-futures]
+                      {:force i :future j :value (value-func supply demand)})]
+    {:forces (into {} indexed-supply)
+     :futures (into {} indexed-futures)
+     :performance results}))
+
+(defn stoke
+  "Given an initial supply, a supply portfolio width, n, and an evaluation 
+   sample size, k, and a sequence of demand futures, generates a map of  
+  
+   {forces  {0 supply1, 1 supply2,...n supplyn}
+    futures {0 future0, 1 future1,...k futurek}
+    results {{[0...n] [0...k] value}}"
+  ([init-supply n k futures] 
+    (supply->portfolio init-supply (take k futures) :n n))
+  ([srcs compos max-end-strength src->strength k futures] 
+    (stoke (->supply-solution srcs compos max-end-strength src->strength))))
+       
+
                                      
