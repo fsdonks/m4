b3d0a8a93aa13b90f28773d91e2802da2bc42c82
Tom Spoon <spoon@blah.net>
2013-10-11 18:10:01 -0400

    Added application stuff...don't remember.

--- src/marathon/processing/stoke/app.clj
+++ src/marathon/processing/stoke/app.clj
@@ -1,3 +1,4 @@
 ;;An interactive application for Stoke.
 (ns marathon.processing.stoke.app
-  (:require [marathon.processing.stoke.core :refer :all]))
+  (:require [marathon.processing.stoke.core :refer :all]
+            [spork.gui.components [swing ))
