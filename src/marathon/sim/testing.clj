b63433e5fb47fd275a417df2180d2914e4a49dbd
Tom Spoon <spoon@blah.net>
2014-08-06 10:46:12 -0400

    Added unit tests for simulation library.

--- src/marathon/sim/testing.clj
+++ src/marathon/sim/testing.clj
@@ -7,6 +7,54 @@
             [marathon.sim.fill.demand   :as fill]
             [marathon.sim.policy :as policy ]
             [marathon.data [simstate :as simstate]]
-            [spork.sim     [simcontext :as sim]]))
+            [spork.sim     [simcontext :as sim]]
+            [clojure.test :refer :all]))
+
+
+
+
+;;Testing for the core Engine Infrastructure
+;;==========================================
+
+;;There is no guard against negative times....we may want to enforce 
+;;that.
+(def ^:constant +end-time+ 1000)
+(def primed-sim
+  (->> (initialize-sim emptysim +end-time+)
+       (sim/add-times [44 100 203 55])))
+
+(deftest basic-engine-testing
+  (is (not (keep-simulating?  emptysim))
+      "We should be able to simulate, since there is time, 
+       and thus events on the clock now.")
+  (is (keep-simulating? primed-sim)
+      "We should be able to simulate, since there is time, 
+       and thus events on the clock now.")
+  (is (not (can-simulate? emptysim))
+      "No supply or demand should indicate as false for now.")
+  (is (= (sim/current-time primed-sim) 1)
+      "Initialized simulation should have a start time of one....")
+  (is (= (sim/current-time primed-sim) 1)
+      "Initialized simulation should have a start time of one....")
+  (is (nil? (sim/current-time emptysim)) "empty simulations have no time")
+  (is (= (sim/get-final-time primed-sim) +end-time+ ) "upper bound should be the final time.")
+  (is (sim/has-time-remaining? primed-sim) "we have time scheduled")
+  (is (not (sim/has-time-remaining? emptysim)) "nothing scheduled, should be no more work.")
+)
+
+
+(defn push-message! [ctx edata name]
+  (let [s    (sim/get-state ctx)
+        msgs (get-in s [:state :messages] [])]
+    (->> (assoc-in s [:state :messages] (conj msgs [name edata]))
+         (sim/set-state ctx))))
+
+(def listener-ctx (sim/make-debug-context :debug-handler push-message!))
+
+(deftest event-propogation-testing
+  
+  
+      
+ 
 
 
