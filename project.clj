e775ae0ee4df280c057925b9b3ee603d2406a4c9
Tom Spoon <spoon@blah.net>
2014-08-28 12:53:28 -0400

    Updated deps to use spork 1.8.2

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure.contrib/standalone "1.3.0-alpha4"]
-                 [spork "0.1.8.1-SNAPSHOT"]]
+                 [spork "0.1.8.2-SNAPSHOT"]]
   :jvm-opts ^:replace ["-Xmx500m" "-XX:NewSize=200m"]
   ;:main marathon.core
   )
