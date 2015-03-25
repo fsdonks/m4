90e7c9402804dbb4c3eed1e4c9e16597b11dee8a
Tom <tom@development>
2015-03-25 00:23:44 -0400

    Fleshing out port of behavior tree.  Documented transition plan

--- src/marathon/data/behavior.clj
+++ src/marathon/data/behavior.clj
@@ -352,13 +352,123 @@
 ;;Legacy Implementation
 ;;======================
 
+;;In an effort to simplify the original implementation for entity
+;;behaviors, I am porting them to composeable behavior trees.
+;;The current task is to determine how to map the existing FSM
+;;into a set of behaviors.
+
+;;The API for the legacy implementation is focused on two functions:
+;;update      :: unitdata -> deltat -> unitdata
+;;changeState :: unitdata -> tostate -> deltat -> *duration ->
+;;              *followingstate -> unitdata
+
+;;Both functions incorporate the notion of possible timedelta;  Both
+;;functions also return the updated unit.
+
+;;update is the more standard usage, as it's purely a function of
+;;time.  The 90% use-case is that the simulation is ticking, prompting
+;;time changes.  We actually have several concurrent simulations (unit
+;;entities) that are flowing through the supply.  The simulation clock
+;;is serving as a central point for coordinating time changes.
+;;As the clock ticks, we advance time.  Units are scheduled for
+;;updating at specific intervals (typically at their request).
+;;The update consists of translating a deltat into an effect on
+;;the unit (and transitively the broader simulation writ large).
+;;So, we have a somewhat complex unit behavior codified as an FSM
+;;that follows a script (a rotational policy), and advances the unit's
+;;position over multiple time-slices (deltat's).  Note that the
+;;time-slices are not arbitrary....for instance the units will request
+;;updates for themselves at eventful points; say according to a
+;;policy, their next move should be to go transition to a new location
+;;and wait for 100 units of time.  They make the state change, i.e.
+;;record the new location, record the fact that they are waiting for
+;;100, and request an update at 100 steps after the current time.
+;;Thus, in the ideal case, the entity will automatically follow its
+;;built-in behavior, which is to move periodically from place to place
+;;as a function of its current policy position and the time in that
+;;position, according to a scripted policy it's following.
+;;This is a happy-medium for re-usable, data-driven behavior, in that
+;;the entity is just a policy-interpreter with a swath of pre-defined
+;;behavior.
+
+;;The other 10% use case is when we need to directly intervene in
+;;an entity's life outside of the passage of time; specifically
+;;an external event "causes" the entity to change its state,
+;;typically to a state determined a-priori.  ChangeState gives us
+;;access to the entity's internal mechanisms to accomplish this,
+;;perhaps via an event or an arbitrary (formerly) method call.
+;;In the case of unit entities, we see this happening when there
+;;are unscheduled deployments, or unplanned withdraws from a demand,
+;;etc.  Basically, we deviate from the current script, and drop the
+;;entity somewhere else in the script.  So long as we stay "somewhere"
+;;in the script, the entity can pick up and continue following along
+;;without caring why, or caring where it came from.
+
+;;In fact, ChangeState is a primitive call for other states to use,
+;;and serves as what would be more commonly found as the familiar,
+;;"onEnter, onUpdate, onExit" set of methods in the State design
+;;pattern.  Other states, like MoveState, are typical targets for
+;;ChangeState, since 99% of the entity's behavior is following a
+;;script that tells it where to move and for how long.
+
+;;If we're thinking of moving to a distributed environment,
+;;We can (probably should?) unify the interface as having each entity
+;;exist as a formal process that receives messages.  In this case,
+;;even time sliced updates are messages.  Currently it's a technical
+;;distinction.
+
+;;The centers of gravity for a useful entity (something that moves of
+;;its own volition, enough for a supply simulation), is to have the
+;;behavior implement (update), and understand how to read from a
+;;script (i.e. the unit's policy).
+
+;;We can generalize and implement an "onEvent" or similar function
+;;later, behind which we can wrap our changestate ops.
+
+;;Also note that many of the states operate not only on the
+;;unit entity, but on the simulation context; in many cases,
+;;information is propogated via the event system (typically
+;;instantaneous events, mostly for logging, ui stuff).
+
+;;So, we are likely to include, either directly or in a
+;;contextual binding, the current simulation context, when
+;;we go to update one or more units.
+;;Transitiong To Behavior Trees
+;;=============================
+;;Changing from the current policy-driven FSM, we need to start at the
+;;choke-points so-to-speak, and then grow from there.  The nice thing
+;;about behaviors is that they compose nicely....so you can build much
+;;more complex behaviors from smaller, simpler behaviors.  We should
+;;also be able to analyze, and possibly "compile" behavior trees at
+;;some point.  For now though, we'll focus on building at least two
+;;fundamental behaviors.
+
+;;The philosophy behind our behavior tree is that the tree contains
+;;all the possible actions a unit entity could take, inlcuding the
+;;sequences and conditions (and joint conditions) therein.  The tree
+;;should structure the logic of our entity's behavior such that its
+;;depth-first traversal embodies the "hierarchy" of concerns for the
+;;entity.  Typically, the AI folks will dictate that this is analagous
+;;to the hierarchy of needs;  higher-priority needs occur earlier in
+;;the tree;  With our naive tree, we always check from the root and
+;;traverse until a stopping criterion is meant;  this means we can
+;;accomplish the same reactive "philosophy" of the existing FSM;
+;;we should be able to make simple changes to the state of our
+;;entity, and the behavior tree - upon re-evaluation - should be
+;;able to suss out what it should be doing.
+
+;;To Be Continued....
+
+
+;;Implementation
+;;==============
 
 
 
 ;;What happens when we change a state?
 ;;Some states are just blips (0 duration) .
 ;;Some states are absorbing states (infinite duration) .
-;;Unless otherwise specified (by a duration), duration is assumed infinite.
+;;Unless otherwise specified (by a duration),e duration is assumed infinite.
 ;; Public Function ChangeState(unit As TimeStep_UnitData, tostate As String, deltat As Single, Optional duration As Single, _
 ;;                                         Optional followingstate As String) As TimeStep_UnitData
 
