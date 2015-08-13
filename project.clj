90d633c7b378616607928e2b2ebb0d44d8022ec3
tom <tom@that-place>
2015-08-13 17:32:01 -0500

    Dropped old contrib dep, upped spork dep to 1.9.5

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject marathon "4.0.8-SNAPSHOT"
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure.contrib/standalone "1.3.0-alpha4"]
-                 [spork "0.1.9.3-SNAPSHOT"]
+;                 [org.clojure.contrib/standalone "1.3.0-alpha4"]
+                 [spork "0.1.9.5-SNAPSHOT"]
                  ]
   :jvm-opts ^:replace ["-Xmx500m" "-XX:NewSize=200m"]
   ;:main marathon.core
