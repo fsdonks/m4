ad56dc89e914ec41c63f067c288a99e963394dd1
tom <tom@that-place>
2016-03-11 11:00:22 -0500

    working on entity beh

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.7.0"]
 ;                 [org.clojure.contrib/standalone "1.3.0-alpha4"]
-                 [spork "0.1.9.6-SNAPSHOT"]
-                 ]
-  :jvm-opts ^:replace ["-Xmx500m" "-XX:NewSize=200m"]
+                 [spork "0.1.9.6-SNAPSHOT"]]
+  :jvm-opts ^:replace ["-Xmx1000m" "-XX:NewSize=200m"]
   ;:main marathon.core
   )
