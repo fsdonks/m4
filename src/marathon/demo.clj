5cd82f1f002698b1816d64eb699185e112d8f57d
tom <tom@that-place>
2016-11-16 03:35:46 -0600

    recorded some samples

--- src/marathon/demo.clj
+++ src/marathon/demo.clj
@@ -62,6 +62,9 @@
 (comment ;testing
   ;;Worked without legacy records...
   (def maxbase "C:\\Users\\tspoon\\Documents\\srm\\tst\\notionalv2\\maxbase.xlsx")
+  (project/audit-project maxbase "C:\\Users\\tspoon\\Documents\\srm\\tst\\notionalv2\\maxbase\\")
+  (run-cases "C:\\Users\\tspoon\\Documents\\srm\\tst\\notionalv2\\" ["maxbase.xlsx"])
+  
   (do-run ep "C:\\Users\\1143108763.C\\srm\\newtest\\")
   (def h
     (a/load-context "C:\\Users\\1143108763.C\\Documents\\srm\\cleaninput\\runs\\srmbase.xlsx"))
