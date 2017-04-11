a2a5c0616d977a381b5b8cf058da500207109dac
tom <tom@that-place>
2017-04-11 18:38:57 -0400

    dos2unix vnv.clj + remove trailing ws

--- src/marathon/vnv.clj
+++ src/marathon/vnv.clj
@@ -20,20 +20,20 @@
               "77202K100"
               "87312K000"]]
    :GSAB    ["GSAB"
-             ["01225K000"]] 
+             ["01225K000"]]
    :CAB     ["CAB"
-             ["01302K000"]]   
+             ["01302K000"]]
    :CSSB    ["CSSB"
-             ["63426K000"]] 
+             ["63426K000"]]
    :QMSUPP  ["QM Supply Co"
-             ["10473R100"]] 
+             ["10473R100"]]
                                         ;     :QMWATER ["QM Water Supp Co"
-                                        ;              ["10460R000"]] 
+                                        ;              ["10460R000"]]
    :QMPOL   ["QM Pol"
              ["10527RA00"
               "10527RC00"
               "10527RF00"
-              "10527RG00"]] 
+              "10527RG00"]]
    :PATRIOT ["ADA Patriot"
              ["44635K000"]] ;in scope
                                         ;  :AVENGER ["ADA Avenger"
@@ -47,7 +47,7 @@
   (do (proc/run-sample! path :interests interests)
       (proc/do-charts-from path :interests interests)))
 
-(comment 
+(comment
 (in-ns 'proc.util)
 (defn read-tsv-dataset
   [path]
@@ -56,24 +56,24 @@
                  (spork.util.table/tabdelimited->records
                   path))]
     (incanter.core/dataset (vec (keys (first rs))) rs)))
-        
+
 (in-ns 'marathon.vnv)
 )
 
 (in-ns 'marathon.ces.engine)
 (defn partial-step
   "Primary state transition function for Marathon.  Threads the next day and
-   an initial state through a series of transfer functions that address 
-   high-level state transfers for supply, policy, demand, filling, and more. 
+   an initial state through a series of transfer functions that address
+   high-level state transfers for supply, policy, demand, filling, and more.
    Computes the resulting state - either the final state, or the initial state
    for the next step."
   ([day ctx]
-   (->> ctx 
+   (->> ctx
         (begin-day        day)  ;Trigger beginning-of-day logic and notifications.
         (manage-supply    day)  ;Update unit positions and policies.
         (manage-policies  day)  ;Apply policy changes, possibly affecting supply.
-        (manage-demands   day)  ;Activate/DeActiveate demands, handle affected units.      
-        ;(fill-demands     day)  ;Try to fill unfilled demands in priority order. 
+        (manage-demands   day)  ;Activate/DeActiveate demands, handle affected units.
+        ;(fill-demands     day)  ;Try to fill unfilled demands in priority order.
         ;(end-day day)           ;End of day logic and notifications.
         ))
   ([ctx] (partial-step (sim/get-time ctx) ctx)))
