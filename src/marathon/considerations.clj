bbe385e52078ae9f196d35e2135f4988e2244aa3
tom <tom@the-cloud>
2017-11-28 02:48:19 +0000

    Added proposal for fixing lifecycle comparisons
    Check marathon.considerations for a detailed look.
    This should address the biases encountered by
    comparing finite to infinite policies, whereby
    infinite policy lengths get shafted due to
    having grossly larger denominators when
    computing normalized dwell.

--- src/marathon/considerations.clj
+++ src/marathon/considerations.clj
@@ -226,3 +226,158 @@
 
 ;;Note: shouldn't be an issue with older policies.
 
+
+
+;;Finite Cycle Length Goal (Comparing Infinite / Finite
+;;Cyclelength Policies Consistently)
+;;=====================================================
+
+;;This is a little simulation to help examine the consequences of
+;;adopting the FCLG(finite cycle-length goal) proposal for solving
+;;comparisons between infinite and finite cycle lengths.
+
+;;So, the default mechanism for comparing entities, at least
+;;the most prevalent, is normalized dwell.  Normalized dwell
+;;arose as an attempt to make the sourcing of units "fair"
+;;regardless of policy cycle.  Unlike the discrete prioritized
+;;fills by compo-pool from older days, which implicitly biased
+;;fills by compo (i.e. drain ac available first, then rc avail, etc.)
+;;we can get a fairer sharing by projecting each entity onto a
+;;normalized cycle length.  This provides a measure of progress
+;;in the cycle as a function of cycletime (nominally a measure of
+;;dwell prior to deployment).  This gave us a more balanced measure
+;;that cut across components.  Note: if policies under comparison
+;;have identical cycle lengths, we effectively get a measure of
+;;dwell, just normalized by the cyclelength....
+
+;;We run into problems when we try to compare "infinite" cyclelength
+;;policies with finite policies.  The problem is that the proxy
+;;for infiniteness is just a massively large integer.  When
+;;comparing against finite cyclelengths, this has the effect
+;;of causing us to have a massiv denominator in the normalizing
+;;computation, leading to a weighting where finite policies
+;;almost always implicitly "Win" when compared to infinite ones.
+;;The only advantage infinite policies offer here is to provide
+;;perhaps earlier deployable times.  The net effect is that
+;;buckets of supply with heterogenous mixtures of finite and
+;;"infinite" policies will end up filling from the finites
+;;first, even if it may not make sense.
+
+;;One proposed solution (A) is to - during comparison - derive a constant
+;;to use as a proxy for the infinite lengths.  The original idea
+;;was to choose a constant from the set of supply under consideration
+;;for sorting, that is, find the max or min cycle-length from the
+;;the entities and use that to compute normalized dwells as needed
+;;iff comparing infinite to finite policies.
+
+;;We'll call this constant the Finite Cycle Length Goal (FCLG).
+
+;;The FCLG is great - we want immutable, consistent things
+;;when trying to derive consistent comparisons.  Yet,
+;;solution A has problems, namely that it's dependent on a
+;;likely variable input - the population of entities being
+;;sorted at a given point in time - which could create
+;;unintentional biases and seemingly variable comparisons.
+;;If the batch has a single outlier, the FCLG now either
+;;penalizes (or benefits) the copmutations for all
+;;finite-to-infinite comparisons.  If the FCLG changes
+;;due to the entry (or exit) of a single entity in the
+;;next fill, we could have the opposite effect.  These
+;;swings could happen throughout.  They make the skin
+;;crawl, since the order of fill is now a variable,
+;;dependent on time and composition, rather than an
+;;constant thing.  If possible, we'd like to tie
+;;our order to something fixed.
+
+;;It turns out we already do this in a sense, when
+;;computing initial conditions for entities following
+;;said infinite policies.  We have to make an assumption
+;;about "where" they are in cycle time at the beginning
+;;of the simulation.  The arrived-upon rule is to detect
+;;effectively infinite cycle lengths (currently lengths over
+;;30 years), and project them along a 3-year, or 1095 day
+;;cyclelength instead.  This keeps the initial conditions
+;;from surging into astronomical regions (say entities
+;;have billions of years of dwell).  The 3-year mark
+;;came from a business rule during the inception of
+;;the infinite length policies....The constant
+;;marathon.ces.entityfactory/+default-cyclelength+
+;;defines this.
+
+;;So, the counter-proposal is simple: We re-use the
+;;bridge between infinite/finite we already established
+;;(for initial conditions), and use the same policy
+;;constant.  The effect is consistent: when comparing
+;;infinite to infinite, we replace one constant (99999999)
+;;with /+default-cyclelength+.  The computed proportions
+;;are just scaled in an affine? manner, and the ordering
+;;should be identical to the original scheme.
+;;When comparing finite->infinite, the rather than having
+;;a biased result against our formerly gigantic demoniator
+;;that effectively zeroed out all of our infinite-length
+;;policies, we have a consistent measure of progress
+;;relative to a 3-year lifecycle, and the ability to
+;;make meaningful inferences about "normalized dwell"
+;;between finite and infinite policies.
+
+;;The following simulation generates random populations
+;;and computes their fill ordering using the old
+;;and new schemes.
+(comment
+  (def inf 9999999)
+  (def fclg 1095)
+  (def policies
+     (->>  [{:policy-name :max
+             :cycle-length inf}
+            {:policy-name :near-max
+             :cycle-length inf}
+            {:policy-name :ac12
+             :cycle-length (* 365 3)}
+            {:policy-name :rc15
+             :cycle-length (* 365 6)}]
+           (mapv #(assoc % :fclg
+                         (if (= (:cycle-length %) inf)
+                           fclg
+                           (:cycle-length %)))))
+    )
+
+  (defn random-unit [id pol]
+    (let [cl (:cycle-length pol)
+          cl (if (== cl inf) fclg cl)]
+      (merge pol {:id id
+                :ct (rand-int cl)
+                }
+           )))
+
+  (defn random-population [n]
+    (for [i (range n)]
+      (random-unit i (rand-nth policies))))
+
+  (defn normalized-dwell [x]
+    (/ (:ct x)
+       (:cycle-length x)))
+
+  (defn fclg-dwell [x]
+    (/ (:ct x)
+       (:fclg x)))
+
+  (defn test [& {:keys [n] :or {n 10}}]
+    (let [xs (random-population n)
+          fs (map #(assoc % :dwell (double (fclg-dwell %))) xs)
+          norms (map #(assoc % :dwell (double (normalized-dwell %))) xs)]
+      {:norms (sort-by (comp - :dwell) norms)
+       :fs    (sort-by (comp - :dwell) fs) }))
+
+  (require '[incanter [core :as i] [charts :as c]])
+  (defn view-test [& {:keys [n] :or {n 10}}]
+    (let [{:keys [norms fs]} (test :n n)]
+      (i/view (c/scatter-plot (range (count norms)) (map :dwell norms)
+                              :title "Normalized Dwell Using CycleLength"
+                              :x-label "Ranked units"
+                              :y-label "Normalized Dwell"))
+      (i/view (c/scatter-plot (range (count fs)) (map :dwell fs)
+                              :title "Normalized Dwell Using Finite CycleLength Goal"
+                              :x-label "Ranked units"
+                              :y-label "Normalized Dwell"))
+      ))
+)
