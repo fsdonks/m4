5e7a9cc4c8afe1b21be571ebb0e7f43d403473ee
tom <tom@the-cloud>
2021-04-08 22:30:33 +0000

    Updated deps, dev profile.

--- project.clj
+++ project.clj
@@ -14,25 +14,25 @@
                  [spork "0.2.1.4-SNAPSHOT"
                   :exclusions [org.clojure/tools.reader]]
                  ;;schemas / specs
-                 [marathon-schemas "4.1.6-SNAPSHOT"
+                 [marathon-schemas "4.1.7-SNAPSHOT"
                   :exclusions [spork]]
                  ;;post processing.
-                 [proc  "0.2.7-SNAPSHOT"
+                 [proc  "0.2.8-SNAPSHOT"
                   :exclusions [spork]]
-                 [stoke "0.1.0-SNAPSHOT"
+                 [stoke "0.1.1-SNAPSHOT"
                   :exclusions [spork]]
-                 [helmet "0.1.0-SNAPSHOT"
+                 [helmet "0.1.1-SNAPSHOT"
                   :exclusions [spork]]
-                 [demand_builder "0.1.0-SNAPSHOT"
+                 [demand_builder "0.1.1-SNAPSHOT"
                   :exclusions [spork]]
                  ;;external libs
                  [joinr/nightclub "0.0.3-SNAPSHOT" :exclusions [clojure.core.async]]
                  [eigenhombre/splasher "0.0.2"] ;;splash screen lib
                  ]
   :jvm-opts ^:replace ["-Xmx4g" #_"-Xmx1000m" "-XX:NewSize=200m"]
-  :source-paths ["src" "../spork/src" "../nightclub/src" "../proc/src"
-                 ]
-  :profiles {:uberjar {;:aot  [marathon.main]
+  :source-paths ["src"]
+  :profiles {:dev {:source-paths ["../spork/src" "../nightclub/src" "../proc/src"]}
+             :uberjar {;:aot  [marathon.main]
                        :aot [marathon.main];~aot-order
                        :main  marathon.main
                        :jvm-opts ^:replace ["-Xmx1000m" "-XX:NewSize=200m" "-server"]
