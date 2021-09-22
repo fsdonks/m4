7088e8a4a6d017ff39fcd384dfab6b40fcaf172a
tom <tom@that-place>
2021-09-22 15:04:20 -0500

    updatd m4 version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  (if (.exists f)
                    (clojure.edn/read-string (slurp "order.edn"))
                    '[marathon.main])))
-(def version "4.2.2")
+(def version "4.2.3")
 (def capsule-name "marathon")
 (def capsule-jar (str  capsule-name "-" version ".jar"))
 
