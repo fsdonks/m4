786c9bee3a131c881480c00461521c0a6faec228
craig <craig.flewelling@gmail.com>
2023-01-30 15:03:11 +0000

    Bumped proc dep with lein git down, version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marathon "4.2.5-SNAPSHOT"
+(defproject marathon "4.2.6-SNAPSHOT"
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [spork "0.2.1.6-SNAPSHOT"
@@ -7,7 +7,7 @@
                  [marathon-schemas "4.1.8-SNAPSHOT"
                   :exclusions [spork]]
                  ;;post processing.
-                 [proc  "0.2.9-SNAPSHOT"
+                 [proc  "0.3.3-SNAPSHOT"
                   :exclusions [spork]]
                  [stoke "0.1.1-SNAPSHOT"
                   :exclusions [spork]]
@@ -16,4 +16,8 @@
                  [demand_builder "0.1.1-SNAPSHOT"
                   :exclusions [spork]]]
   :jvm-opts ^:replace ["-Xmx4g" "-XX:NewSize=200m"]
-  :source-paths ["src"])
+  :source-paths ["src"]
+  :plugins [[reifyhealth/lein-git-down "0.4.1"]]
+  :middleware [lein-git-down.plugin/inject-properties]
+  :repositories [["public-github" {:url "git://github.com"}]]
+  :git-down {proc  {:coordinates  fsdonks/proc}})
