d31e0fddc7b7f864b9c70b18d64999db6776c59d
craig <craig.flewelling@gmail.com>
2023-05-16 15:53:33 +0000

    version bumps for small spork and demand_builder patches.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 ;;Go to marathon.core to update +version+ as well!
-(defproject marathon "4.2.10-SNAPSHOT"
+(defproject marathon "4.2.11-SNAPSHOT"
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [spork "0.2.1.6-SNAPSHOT"
+                 [spork "0.2.1.7-SNAPSHOT"
                   :exclusions [org.clojure/tools.reader]]
                  ;;schemas / specs
                  [marathon-schemas "4.1.8-SNAPSHOT"
@@ -14,11 +14,12 @@
                   :exclusions [spork]]
                  [helmet "0.1.1-SNAPSHOT"
                   :exclusions [spork]]
-                 [demand_builder "0.1.1-SNAPSHOT"
+                 [demand_builder "0.1.2-SNAPSHOT"
                   :exclusions [spork]]]
   :jvm-opts ^:replace ["-Xmx4g" "-XX:NewSize=200m"]
   :source-paths ["src"]
   :plugins [[reifyhealth/lein-git-down "0.4.1"]]
   :middleware [lein-git-down.plugin/inject-properties]
   :repositories [["public-github" {:url "git://github.com"}]]
-  :git-down {proc  {:coordinates  fsdonks/proc}})
+  :git-down {proc  {:coordinates  fsdonks/proc}
+             demand_builder  {:coordinates  fsdonks/demand_builder}})
