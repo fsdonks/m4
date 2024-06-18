1cdd2ebff96f986a290dc0eaaa7d46a9fd3e9aa7
craig <craig.flewelling@gmail.com>
2024-06-18 17:37:47 +0000

    dailydeployed-added ranked training days and ranking by :src only.

--- src/marathon/analysis/dailydeployed.clj
+++ src/marathon/analysis/dailydeployed.clj
@@ -34,21 +34,35 @@
     (cond demand-name (:demandgroup demand-name)
           (:deployable u) :deployable
           :else :not-deployable)))
-      
-(defn unit->record [u ctx t rankings]
+
+(defn t-level
+  [src rank trainings]
+  (let [indices (trainings src)
+        [idx limit :as indexed]
+        (first (filter #(> (second %) rank) indices))]
+    (if indexed
+      (inc idx)
+      (throw 
+       (ex-info "Inventory might be greater than the number of units in
+  training-days."
+                {:src src :rank rank :indices indices})))))
+
+(defn unit->record [u ctx t rankings trainings]
   (let [{:keys [src name policy]} u
         policy-name (get-in policy [:activepolicy :name])
-        position (unit->location u ctx)]
+        position (unit->location u ctx)
+        rank (rankings name)]
     {:name name
      :src src
      :day t
      :position position
-     :rank (rankings name)
+     :rank rank
      :unit-or-demand :unit
      :demand-group (if (contains? #{:deployable :not-deployable}
                                   position)
                      nil
-                     position)}))
+                     position)
+     :t-level (t-level src rank trainings)}))
 
 (defn start-deployable [u]
   (get-in u [:policy :activepolicy :startdeployable]))
@@ -79,40 +93,52 @@
 ;;group units by src, compo, sort the groups, each unit gets a rank
 (defn rank-units
   "Given a sequence of unit entities, returns a map of the unit's name
-  to it's rank among the the units grouped by group-fn and sorted by
+  to it's rank among the the units grouped by src and sorted by
   rank-fn."
-  [units group-fn rank-fn]
-  (let [groups (group-by group-fn units)]
+  [units rank-fn]
+  (let [groups (group-by :src units)]
     (into {} (mapcat #(grouped-rank %) groups))))
 
-(defn src-compo
-  [unit]
-  ((juxt :src :component) unit))
+(defn training-indices
+  [train-days]
+  (reduce-kv (fn [acc src train-days]
+               (assoc acc src
+                      (map-indexed vector
+                                   (reductions + train-days))))
+               {}
+               training-days))
 
 ;;Per vstats, after calling current-units to add a dt to the unit,
 ;;need to update unit cycletime.
-(defn frame->dailydeployed [[t ctx]]
+(defn frame->dailydeployed [[t ctx] training-days]
   (let [units (map #(update % :cycletime + (% :dt))
                    (c/current-units ctx))
-        rankings (rank-units units src-compo deployable-portion)]
-    (map #(unit->record % ctx t rankings) units)))
+        rankings (rank-units units deployable-portion)
+        trainings (training-indices training-days)]
+    (map #(unit->record % ctx t rankings trainings) units)))
 
 (defn daily-deployed
   "Returns a table with keys
-  [:src :demand-group :day :unit-or-demand :deployed-c-level] where
+  [:src :demand-group :day :unit-or-demand :position] where
   :state-or-demand is :deployable, :not-deployable, or a
   demand group.
-  :unit-or-demand is always a unit name (index_src_compo).
-  :deployed-t-level is always the deployed t-level derived from
-  cycletime and from the
-  name of the policy with days to T1 from T3 and days to T1 from
-  T2. We'll assume T4 were cannibalized."
-  [proj]
+  :unit-or-demand is always :unit.
+  :demand-group is the demand group if the unit is deployed.
+  Otherwise, it is nil.
+  :position is either the demand-group, :not-deployable, or
+  :deployable.
+  :rank is the unit's position in a sequence when grouped by [src
+  compo] and sorted by cycletime/startdeployable"
+  [proj training-days]
   (let [stream (lerp-stream (a/marathon-stream proj))]
-    (mapcat frame->dailydeployed stream)))
+    (mapcat #(frame->dailydeployed % training-days) stream)))
+
+(def training-days
+  {"77202K000" [1 2 3 200]
+   "01205K000" [20 40 1 200]})
 
 (def test-path "/home/craig/runs/test-run/testdata-v7-bog.xlsx")
-(def dailys (daily-deployed test-path))
+(def dailys (daily-deployed test-path training-days))
 (def frame1 (second (a/marathon-stream test-path)))
 (def ctx1 (second frame1))
 (def unit1 (first (c/units ctx1)))
