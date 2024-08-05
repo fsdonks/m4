5f7145d69e87049d5e49d1215a4d7aaddef19b1d
tom <tom@that-place>
2024-08-05 14:15:58 -0500

    4.2.19-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;;Go to marathon.core to update +version+ as well!
-(defproject marathon "4.2.18-SNAPSHOT"
+(defproject marathon "4.2.19-SNAPSHOT"
   :description "An Integrated Suite of Rotational Analysis Tools."
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [spork "0.2.1.8-SNAPSHOT"
